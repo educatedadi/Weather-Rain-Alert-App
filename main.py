a5a0f3d504165c02f2b21305f645c555a0c24885
Aditya <93342026+educatedadi@users.noreply.github.com>
2022-01-22 21:35:29 +0530

    OS Update

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import requests
 from twilio.rest import Client
+import os
 
-my_email = "adisenpai101@gmail.com"
-password = "ar_@12345"
-
-MY_API = "43714508be1015dd968c7fbbc552f7b1"
-MY_LAT = 24.047930  # My Latitude
-MY_LONG = 84.069099  # My Longitude
+MY_API = os.environ.get("OWS_API")
+MY_LAT = 51.507351  # Your Latitude
+MY_LONG = -0.127758  # Your Longitude
 
 twilio_sid = "AC8be37413d120bb67e05b025c41e8a578"
-auth_token = "a8f23380f3e0a4712f93e7babc5d9943"
+auth_token = os.environ.get("AUTH_TOKEN")
 
-deff_raining_lat = -18.157132
-deff_raining_long = 49.409860
+verified_mobile_number = os.environ.get("PHONE_NUM")
 
 parameters = {
     "lat": deff_raining_lat,
@@ -43,7 +40,7 @@ if will_rain:
 
     message = client.messages.create(
         body="It's going to rain today☔☔",
-        to='+919135620787',
+        to=verified_mobile_number,
         messaging_service_sid='MGce06d87f3acd11cc67484be81dca8182',
     )
 
